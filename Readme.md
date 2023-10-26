a6c9618f6c5fe8c6d34ff7ec405a98fcc493fed4
hejj16 <dujiaoxianqiaojia@126.com>
2023-10-26 12:18:35 +0100

    update env

--- Readme.md
+++ Readme.md
@@ -9,7 +9,8 @@ We suggest using the following commands.
 ```
 conda create --name $ENV_NAME
 conda activate $ENV_NAME
-
+conda install pytorch=2.1.0 torchvision=0.16.0 torchaudio=2.1.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+pip3 install -r requirements.txt
 ```
 
 
@@ -18,7 +19,8 @@ conda activate $ENV_NAME
 ### Kodak
 
 Please put training images and test images into ```train_dir``` and ```test_dir``` respectively.
-For the training set, we suggest using [DIV2K](https://data.vision.ee.ethz.ch/cvl/DIV2K/) cropped to the same size as the Kodim images.
+For the training set, we suggest using [DIV2K](https://data.vision.ee.ethz.ch/cvl/DIV2K/). 
+To align with the resolution of Kodak, the training images should be randomly cropped into patches with resolution of 512x768 or 768x512.
 
 
 ### CIFAR-10
--- dataset.py
+++ dataset.py
@@ -10,44 +10,10 @@ from PIL import Image
 import pickle
 from torchvision.transforms import ToTensor
 from torchvision import io, transforms
-
-# 
-def make_coord_grid(shape, range, device=None):
-    """
-        Args:
-            shape: tuple
-            range: [minv, maxv] or [[minv_1, maxv_1], ..., [minv_d, maxv_d]] for each dim
-        Returns:
-            grid: shape (*shape, )
-    """
-    l_lst = []
-    for i, s in enumerate(shape):
-        l = (0.5 + torch.arange(s, device=device)) / s
-        if isinstance(range[0], list) or isinstance(range[0], tuple):
-            minv, maxv = range[i]
-        else:
-            minv, maxv = range
-        l = minv + (maxv - minv) * l
-        l_lst.append(l)
-    grid = torch.meshgrid(*l_lst)
-    grid = torch.stack(grid, dim=-1)
-    return grid
-
-
-def to_grid_coordinates_and_features(img):
-    """Converts an image to a set of coordinates and features.
-    Args:
-        img (torch.Tensor): Shape (channels, height, width).
-    """
-    # Coordinates are indices of all non-zero locations of a tensor of ones of
-    # same shape as spatial dimensions of image
-
-    coordinates = make_coord_grid(img.shape[1:], (-1, 1), device=img.device).view(-1, len(img.shape[1:]))
-    # Convert image to a tensor of features of shape (num_points, channels)
-    features = img.reshape(img.shape[0], -1).T
-    return coordinates, features
+from utils import to_grid_coordinates_and_features
 
 # process video dataset
+# the parts mapping coordinates to fourier embeddings are modified from COMBINER (https://github.com/cambridge-mlg/combiner)
 def process_video_datasets(train_paths, test_paths, save_dir):
     """
     This function processes UCF-101 dataset and store training set and test set into list of tensors.
@@ -61,7 +27,7 @@ def process_video_datasets(train_paths, test_paths, save_dir):
         video_tensor = []
         for file_name in path:
             # follow VC-INR paper (https://arxiv.org/abs/2301.09479)
-            # for each video, clip out the first 24 frames, center crop by (240, 240) and reshape it to (128, 128) 
+            # for each video, take the first 24 frames, center crop by (240, 240) and reshape it to (128, 128) 
             video = io.read_video(file_name)[0].permute([0, 3, 1, 2])[:24, ...]
             if video.shape[-1] >= 240 and video.shape[-2] >= 240:
                 video = transforms.CenterCrop([240, 240])(video)
@@ -75,58 +41,57 @@ def process_video_datasets(train_paths, test_paths, save_dir):
     with open(save_dir + '/train_dataset.pkl', 'wb') as f:
         pickle.dump(train_tensor, f)
 
-
-
-def get_ucf_pair(tensor, # T, C(3), W, H
-                 feature_size=None,
-                 patch=False,
-                 patch_size=None):
+def get_video_pair(tensor, # Time, C(3), W, H
+                   feature_size=None,
+                   patch=False,
+                   patch_sizes=None):
     tensor = tensor.permute([1, 0, 2, 3])
     c, t, x, y = tensor.shape
+    data_dim = len(tensor.shape) - 1
 
     if not patch:
         inputs, outputs = to_grid_coordinates_and_features(tensor)
-        w = torch.exp(torch.linspace(0, np.log(1024), feature_size // 4, device=inputs.device))
+        w = torch.exp(torch.linspace(0, np.log(1024), feature_size // (2*data_dim), device=inputs.device))
         inputs = torch.matmul(inputs.unsqueeze(-1), w.unsqueeze(0)).view(*inputs.shape[:-1], -1)
         inputs = torch.cat([torch.cos(np.pi * inputs), torch.sin(np.pi * inputs)], dim=-1)
         return inputs, outputs
     else:
         Inputs = []
         Outputs = []
-        for x_idx in range(x // patch_size):
-            for y_idx in range(y // patch_size):
-                patch = tensor[...,
-                        x_idx * patch_size: x_idx * patch_size + patch_size,
-                        y_idx * patch_size: y_idx * patch_size + patch_size
-                        ]
-                inputs, outputs = to_grid_coordinates_and_features(patch)
-                w = torch.exp(torch.linspace(0, np.log(1024), feature_size // 6, device=inputs.device))
-                inputs = torch.matmul(inputs.unsqueeze(-1), w.unsqueeze(0)).view(*inputs.shape[:-1], -1)
-                inputs = torch.cat([torch.cos(np.pi * inputs), torch.sin(np.pi * inputs)], dim=-1)
-                Inputs.append(inputs)
-                Outputs.append(outputs)
+        for t_idx in range(t // patch_sizes[0]):
+            for x_idx in range(x // patch_sizes[1]):
+                for y_idx in range(y // patch_sizes[2]):
+                    patch = tensor[...,
+                                   t_idx * patch_sizes[0]: t_idx * patch_sizes[0] + patch_sizes[0],
+                                   x_idx * patch_sizes[1]: x_idx * patch_sizes[1] + patch_sizes[1],
+                                   y_idx * patch_sizes[2]: y_idx * patch_sizes[2] + patch_sizes[2]
+                                   ]
+                    inputs, outputs = to_grid_coordinates_and_features(patch)
+                    w = torch.exp(torch.linspace(0, np.log(1024), feature_size // (2*data_dim) , device=inputs.device))
+                    inputs = torch.matmul(inputs.unsqueeze(-1), w.unsqueeze(0)).view(*inputs.shape[:-1], -1)
+                    inputs = torch.cat([torch.cos(np.pi * inputs), torch.sin(np.pi * inputs)], dim=-1)
+                    Inputs.append(inputs)
+                    Outputs.append(outputs)
         Inputs = torch.stack(Inputs)
         Outputs = torch.stack(Outputs)
         return Inputs, Outputs
 
-
-
-def load_ucf(video_tensors, feature_size, patch, patch_size):
+def load_video(video_tensors, feature_size, patch, patch_sizes):
     # data
     X = []
     Y = []
     for i in video_tensors:
-        x, y = get_ucf_pair(i,
-                            feature_size=feature_size,
-                            patch=patch,
-                            patch_size=patch_size
-                            )
+        x, y = get_video_pair(i,
+                              feature_size=feature_size,
+                              patch=patch,
+                              patch_sizes=patch_sizes
+                              )
         if patch:
             X.append(x)
             Y.append(y)
         else:
-            X.append(x[None, :, :])
-            Y.append(y[None, :, :])
+            X.append(x[None, ...])
+            Y.append(y[None, ...])
     return torch.cat(X, dim=0), torch.cat(Y, dim=0)
 
 
